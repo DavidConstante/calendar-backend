ad31725cf2e49667288e4efb9401c92b32e534c9
DavidConstante <omileconstante@gmail.com>
2022-09-22 16:26:11 -0500

    Edit the gitignore to upload the .env

--- database/config.js
+++ database/config.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 const dbConnection = async () => {
   try {
-    await mongoose.connect(process.env.DB_CNN)
+    // await mongoose.connect(process.env.DB_CNN)
+    await mongoose.connect('mongodb+srv://sysdba:toor@calendardb.gaum6pd.mongodb.net/mern-calendar')
     console.log('DB Online')
 
   } catch (error) {
